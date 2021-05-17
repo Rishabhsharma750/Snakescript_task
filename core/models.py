8c39aceb1af7e668bdfddd8e48a6b776ad697832
Lenovo <rishabhsharma750@gmail.com>
2021-05-17 12:18:54 +0530

    updated

---
+++ core/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Profession(models.Model):
+    description=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.description
+
+class DataSheet(models.Model):
+    description=models.CharField(max_length=50)
+    historical_data=models.TextField()
+
+    def __str__(self):
+        return self.description
+
+class Customer(models.Model):
+    name=models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
+    professions=models.ManyToManyField(Profession)
+    data_Sheet=models.OneToOneField(DataSheet,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+class Document(models.Model):
+    PP='PP'
+    ID ='ID'
+    OT='OT'
+
+    DOC_TYPE=(
+        (PP,'Passport'),
+        (ID,'Identity Card'),
+        (OT,'Other')
+    )
+
+    dtype=models.CharField(choices=DOC_TYPE,max_length=2)
+    doc_number=models.CharField(max_length=50)
+    customers=models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.doc_number
